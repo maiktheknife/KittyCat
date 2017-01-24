0f16be25d8156d6ec92e259c36e81397803c6192
maiktheknife <maiktheknife@gmail.com>
2017-01-24 20:20:01 +0100

    add LeakCanary

--- app/src/main/kotlin/net/kivitro/kittycat/App.kt
+++ app/src/main/kotlin/net/kivitro/kittycat/App.kt
@@ -1,6 +1,7 @@
 package net.kivitro.kittycat
 
 import android.app.Application
+import com.squareup.leakcanary.LeakCanary
 import net.kivitro.kittycat.network.TheCatAPI
 import net.kivitro.kittycat.util.UIUtil
 import timber.log.Timber
@@ -12,9 +13,14 @@ class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        setUpLeakCanary()
         setUpTimber()
         setUpRetrofit()
         UIUtil.setUpTheme(this)
+    }
+
+    private fun setUpLeakCanary(){
+        LeakCanary.install(this);
     }
 
     private fun setUpTimber() {
